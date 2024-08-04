ba3680346dc3913124e6db37c8b6d3cd3ad29347
rshindooo <rshindooo@gmail.com>
2024-08-03 23:29:08 -0700

    Some changes

--- backend/passenger_cluster/urls.py
+++ backend/passenger_cluster/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from django.views.generic import TemplateView
 urlpatterns = [
-    path('', views.index, name='index'),
+    # ... your other URL patterns ...
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
