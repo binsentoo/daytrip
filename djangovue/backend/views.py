dcc86792d7bcd9279f6faab2cb3a35271333cedf
Vincent Phan <86548242+binsentoo@users.noreply.github.com>
2024-08-24 18:42:00 -0700

    Implements working buttons
    Organized js and vue files into folders

--- djangovue/backend/views.py
+++ djangovue/backend/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request): 
   return HttpResponse(render(request, 'vue_index.html'))
+
+def login(request): 
+  return HttpResponse(render(request, 'login.html'))
--- djangovue/frontend/tailwind.config.js
+++ djangovue/frontend/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["build/templates/**/*.{html,js,vue}", "build/templates/*.{html,js,vue}", "src/*.{html,js,vue}"],
+  content: ["build/templates/**/*.{html,js,vue}", 
+            "build/templates/*.{html,js,vue}", 
+            "src/*.{html,js,vue}",
+            "src/components/*.{html,js,vue}"
+  ],
   theme: {
     extend: {},
   },
