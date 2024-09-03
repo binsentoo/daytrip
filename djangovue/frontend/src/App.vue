7da2e6b3fcbd2196e18c6e7a24fab012428c3444
Vincent Phan <86548242+binsentoo@users.noreply.github.com>
2024-09-03 15:08:45 -0700

    Fixed header component loading issue

--- djangovue/frontend/src/App.vue
+++ djangovue/frontend/src/App.vue
@@ -1,5 +1,10 @@
-<script setup>
+<script>
 import HeaderComp from './components/HeaderComp.vue';
+export default {
+  components: {
+    HeaderComp
+  }
+}
 </script>
 
 <template>
--- djangovue/frontend/src/js/login.js
+++ djangovue/frontend/src/js/login.js
@@ -1,6 +1,4 @@
 import { createApp } from 'vue'
 import App from '../components/LoginApp.vue'
-import Header from '../components/HeaderComp.vue'
 
 createApp(App).mount('#LoginApp')
-createApp(Header).mount('#header')
