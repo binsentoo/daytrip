bd0dd9b50cda1a79e01576378070e26e49f9eda4
Vincent Phan <86548242+binsentoo@users.noreply.github.com>
2024-09-08 01:41:19 -0700

    Fixed

--- djangovue/frontend/vue.config.js
+++ djangovue/frontend/vue.config.js
@@ -1,17 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
-const pages = {  
-  index: "src/js/main.js",
-  login: "src/js/login.js"
-};  
-  
+
 module.exports = {  
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/production-sub-path/'
-    : '/dist',
+    ? '/'
+    : '/dist/',
   outputDir: "dist",
   indexPath: "index.html",
   
-  pages: pages,  
-  
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/js/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+    },
+    // when using the entry-only string format,
+    // template is inferred to be `public/subpage.html`
+    // and falls back to `public/index.html` if not found.
+    // Output filename is inferred to be `subpage.html`.
+    login: 'src/js/login.js'
+  }
 };
