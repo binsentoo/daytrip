61e200728e9b22acec0f09d8e338ba071c7d6663
Vincent Phan <86548242+binsentoo@users.noreply.github.com>
2024-09-11 17:50:39 -0700

    Update settings.py

--- djangovue/backend/settings.py
+++ djangovue/backend/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'dist/'
+STATIC_URL = '/dist/'
 
 AUTH_USER_MODEL = 'auth.User'
 
