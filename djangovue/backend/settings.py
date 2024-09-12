c8cf61cd29b5faa8fe6d40db5178c6914328eaac
Vincent Phan <86548242+binsentoo@users.noreply.github.com>
2024-09-11 18:19:43 -0700

    Update settings.py

--- djangovue/backend/settings.py
+++ djangovue/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = str(os.environ.get('DJANGO_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
