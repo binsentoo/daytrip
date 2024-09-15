2f2267e6bea017fcf78b9d3393876a8631d52e7e
rshindooo <rshindooo@gmail.com>
2024-09-15 13:58:36 -0700

    final

--- djangovue/frontend/src/pages/LoginApp.vue
+++ djangovue/frontend/src/pages/LoginApp.vue
@@ -1,1 +1,18 @@
-hello world!
+<template>
+  <div>
+    <h1>Hello World!</h1>
+    <p>Welcome to your Vue.js app.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginApp',
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
