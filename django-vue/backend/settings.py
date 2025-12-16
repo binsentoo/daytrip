7f489b4d32993af3620c79056cd4cd1293cc3d0a
Vincent Phan <86548242+binsentoo@users.noreply.github.com>
2025-12-15 21:01:29 -0800

    add csrf capability

--- django-vue/backend/settings.py
+++ django-vue/backend/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,12 +58,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "https://daytrip-kappa.vercel.app"
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://daytrip-kappa.vercel.app",
 ]
 
 ROOT_URLCONF = 'backend.urls'
