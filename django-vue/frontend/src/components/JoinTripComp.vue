5a68cc62aee71e65a9ef84a56717ea1af00df2de
Vincent Phan <86548242+binsentoo@users.noreply.github.com>
2025-12-15 21:28:38 -0800

    fix code object to string

--- django-vue/frontend/src/components/JoinTripComp.vue
+++ django-vue/frontend/src/components/JoinTripComp.vue
@@ -8,7 +8,7 @@ const code = ref('')
 async function getDaytrip() {
     try {
         const API_BASE = import.meta.env.VITE_API_BASE || 'http://127.0.0.1:8000'
-        const response = await axios.get(`${API_BASE}/api/daytrips/${code}/`)
+        const response = await axios.get(`${API_BASE}/api/daytrips/${code.value}/`)
         router.push(`/daytrip/${code.value}`)
         return response.data
     } catch(error) {
